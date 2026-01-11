a70bf89e9680fd8e3c37d382865160f9b00e553c
kristofajosh <christopherjoshua18@live.com>
2026-01-11 06:10:31 +0100

    Add Easter email template and update preview props.

--- emails/general/easter.tsx
+++ emails/general/easter.tsx
@@ -5,41 +5,41 @@ import { Link } from "@/components/link";
 import { Typography } from "@/components/typography";
 import { useEmailVariables } from "@/lib/context/variable-context";
 
-export interface {{pascalCase folder}}{{pascalCase componentName}}Props {
+export interface GeneralEasterProps {
   email: string;
   name?: string;
 }
 
-const EmailBody = ({ name }: {{pascalCase folder}}{{pascalCase componentName}}Props) => {
+const EmailBody = ({ name }: GeneralEasterProps) => {
   const { baseHref } = useEmailVariables();
 
   return (
     <div className="content-body">
       {name ? <Typography>Hi {name},</Typography> : null}
       <Typography>
-        This is the {{sentenceCase componentName}} email.
+        This is the Easter email.
       </Typography>
       <br />
-      <Link href={baseHref('{{buttonPath}}')}>this is a link</Link>
+      <Link href={baseHref('/account/general')}>this is a link</Link>
       <br />
-      <Button href={baseHref('{{buttonPath}}')}>
+      <Button href={baseHref('/account/general')}>
         Goto Dashboard
       </Button>
     </div>
   );
 };
 
-export const {{pascalCase componentName}} = ({ ...props }: {{pascalCase folder}}{{pascalCase componentName}}Props) => {
+export const Easter = ({ ...props }: GeneralEasterProps) => {
   return (
-    <EmailContentWrapper email={props.email} previewText="{{previewText}}">
+    <EmailContentWrapper email={props.email} previewText="happy easter">
       <EmailBody {...props} />
     </EmailContentWrapper>
   );
 };
 
-{{pascalCase componentName}}.PreviewProps = {
+Easter.PreviewProps = {
   email: "notify@org.com",
   name: "John Doe",
-} as {{pascalCase folder}}{{pascalCase componentName}}Props;
+} as GeneralEasterProps;
 
-export default {{pascalCase componentName}};
+export default Easter;
